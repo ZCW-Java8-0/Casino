c47d807efeac8c91443575f9bef1900749c4e518
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-03-14 13:56:25 -0400

    UML

--- src/main/java/com/github/zipcodewilmington/casino/GameInterface.java
+++ src/main/java/com/github/zipcodewilmington/casino/GameInterface.java
@@ -3,12 +3,20 @@ package com.github.zipcodewilmington.casino;
 /**
  * Created by leon on 7/21/2020.
  */
-public interface GameInterface extends Runnable {
+public interface GameInterface<PlayerType extends PlayerInterface> extends Runnable {
+    Boolean isOver();
+
     /**
      * adds a player to the game
-     * @param player the player to be removed from the game
+     * @param player the player to be added to the game
      */
-    void add(PlayerInterface player);
+    void add(PlayerType player);
+
+    /**
+     * adds players to the game
+     * @param player the players to be added to the game
+     */
+    void addPlayers(Iterable<? extends PlayerType> player);
 
     /**
      * removes a player from the game
