a46d60bb6ab3c711253801ffba46482ae4db31de
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-15 20:33:20 -0400

    committing progress on WarGame class

--- src/main/java/com/github/zipcodewilmington/casino/games/war/WarGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/war/WarGame.java
@@ -2,6 +2,7 @@ package com.github.zipcodewilmington.casino.games.war;
 
 import com.github.zipcodewilmington.casino.Game;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -13,21 +14,34 @@ public class WarGame extends Game {
     // I don't think I need a main method here as the game will be initiated in another class.
 
     public void warGame() { // ROUGH DRAFT
-        // import deck (stack) (shuffled)
+        // import deck (stack/deque) (shuffled)
         // player1 = whoever
         // player2 = whoever else
-        Stack<Deck> handPlayer1 = new Stack<Deck>();
-        Stack<Deck> handPlayer2 = new Stack<Deck>();
+        Deque<Deck> handPlayer1 = new ArrayDeque<Deck>(); // Stack<Deck> handPlayer1 = new Stack<Deck>();
+        Deque<Deck> handPlayer2 = new ArrayDeque<Deck>(); // Stack<Deck> handPlayer2 = new Stack<Deck>();
         for (int i = 1; i <= 26; i++) { // deal cards
-            handPlayer1.push(deck.pop());
-            handPlayer2.push(deck.pop());
+            handPlayer1.addFirst(deck.removeFirst());
+            handPlayer2.addFirst(deck.removeFirst());
         }
 
         while (handPlayer1.size() < 52 && handPlayer2.size() < 52) {
+            Deck player1Card = handPlayer1.removeFirst();
+            Deck player2Card = handPlayer2.removeFirst();
+            if(player1Card > player2Card) {
+                handPlayer1.addLast(player1Card);
+                handPlayer1.addLast(player2Card);
+            } else if (player1Card < player2Card) {
+                handPlayer2.addLast(player1Card);
+                handPlayer2.addLast(player2Card);
+            } else if (player1Card == player2Card) {
+                // TIE
+            }
             // player input - play card
             // if p1 > p2 vs p2>p1 vs tie - player output show card and winner
             // cards to winner
             // display hand size i.e. 26/52
+            // pop stack(deck) to pull card
+            // code for lose - one person is out of cards
         }
 
         if (handPlayer1.empty()) {
@@ -40,12 +54,12 @@ public class WarGame extends Game {
 
     // 2 players or 1v computer
     // how do they quit midgame?
-    // war is a non-gambling game, right?
+    // war is a non-gambling game, right? CORRECT
 
 
     // try test driven development - but hard since don't know where each piece is going
     // review generics lecture - Wei posted slides in watercrest, vandana got recording
-    // pop stack(deck) to pull card
+
 
 
 }
