da6deadfae983a8d13b311424e1a985039c4cdb5
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-16 08:31:06 -0400

    finish deck

--- src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/Deck.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/Deck.java
@@ -1,5 +1,26 @@
 package com.github.zipcodewilmington.casino.games.BlackJack;
 
-public class Deck {
+import java.util.Collections;
+import java.util.Stack;
 
+public class Deck {
+    private Stack<Card> deck;
+
+    public Deck(){
+        this.deck = new Stack<>();
+        for (int i=0; i<13;i++)
+        {
+            CardFace face = CardFace.values()[i];
+            for (int j=0; j<4; j++)
+            {
+                Card card = new Card(face, Suit.values()[j]);
+                this.deck.add(card);
+            }
+        }
+        Collections.shuffle(deck);
+    }
+
+    public Card getTopCard(){
+        return deck.pop();
+    }
 }
