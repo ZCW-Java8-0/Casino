3495ea8063a78090af3486b6dc1450e26ea41a1e
Vandana <vandana@Zipcoders-MBP.fios-router.home>
2022-03-16 09:18:05 -0400

    random and spinWheel methods created

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/Roulette/RoulettePlayer.java
@@ -0,0 +1,26 @@
+package com.github.zipcodewilmington.casino.games.Roulette;
+
+import com.github.zipcodewilmington.casino.games.Person.Gambler;
+import com.github.zipcodewilmington.casino.games.Person.Person;
+import com.github.zipcodewilmington.casino.games.Person.Player;
+
+public class RoulettePlayer extends Player implements Gambler {
+    public RoulettePlayer ( Person person ) {
+        super(person);
+    }
+
+    @Override
+    public void addWinning ( int winning ) {
+
+    }
+
+    @Override
+    public void applyBet ( int bet ) {
+
+    }
+
+    @Override
+    public int getBalance () {
+        return 0;
+    }
+}
