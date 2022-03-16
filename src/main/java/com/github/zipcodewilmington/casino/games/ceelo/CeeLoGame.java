1fac34ea7c89e58918899ae6e2b28a932e92447d
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-16 00:15:40 -0400

    dice outcomes

--- src/main/java/com/github/zipcodewilmington/casino/games/ceelo/CeeLoGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/ceelo/CeeLoGame.java
@@ -1,4 +1,9 @@
 package com.github.zipcodewilmington.casino.games.ceelo;
 
-public class CeeLoGame {
+public class CeeLoGame  {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to CeeLo!");
+
+    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/ceelo/Dice.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/ceelo/Dice.java
@@ -4,9 +4,9 @@ package com.github.zipcodewilmington.casino.games.ceelo;
 import java.util.Random;
 
 public class Dice {
-    public static int[] rollDice(){
+    public static int[] rollDice(Random diceNumber){
     int[] eachDice = new int[3];
-    Random diceNumber= new Random();
+//    Random diceNumber= new Random();
 
     for (int i = 0; i< eachDice.length; i++) {
         eachDice[i] = diceNumber.nextInt(1,7);
