fb74d2f8cb907ffb93fdbd45f6a39aca21c809c9
Mike <mike@Zipcoders-MacBook-Pro.local>
2022-03-17 14:09:14 -0400

    added numbergame logic

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -3,11 +3,47 @@ package com.github.zipcodewilmington.casino.games.numberguess;
 import com.github.zipcodewilmington.casino.GameInterface;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class NumberGuessGame implements GameInterface<NumberGuessPlayer> {
+
+    Scanner scanner = new Scanner(System.in); //Scanner Class
+    int num = 1 + (int) (10 * Math.random());// Computer Choice
+    int guessCount = 0;
+    int previousGuess = 11;
+    //Actual Game
+        while(true)
+
+    {
+
+        {
+            System.out.println("Pick a number between 1 and 10");
+            int guess = scanner.nextInt();
+            if (guess == num) {
+                guessCount += 1;
+                System.out.println("Congratulations! You guessed the number!");
+                System.out.println("You guessed in" + " " + guessCount + " " + "tries!");
+                break;
+            } else if (guess > num) {
+                System.out.println("Guess is too high, pick again");
+                if (previousGuess != guess) {
+                    guessCount += 1;
+                    previousGuess = guess;
+                }
+            } else {
+                System.out.println("Guess is too low, pick again");
+                if (previousGuess != guess) {
+                    guessCount += 1;
+                    previousGuess = guess;
+                }
+            }
+            scanner.close();
+        }
+    }
+
 
 
     @Override
