59766cf8aabf612b6003fd69fdcc2ab743ec943e
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-18 15:50:23 -0400

    extracted some methods

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJackTest.java
@@ -0,0 +1,32 @@
+package com.github.zipcodewilmington.casino.games.BlackJack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlackJackTest {
+
+    @Test
+    void dealerPlays() {
+    }
+
+    @Test
+    void displayCard() {
+    }
+
+    @Test
+    void blackJackFlag() {
+    }
+
+    @Test
+    void winConditionCheck() {
+    }
+
+    @Test
+    void setBets() {
+    }
+
+    @Test
+    void distributeWinningsToWinners() {
+    }
+}
