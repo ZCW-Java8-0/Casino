ddc4668691c74b54e4bfd0cdccd8d2e97e789766
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-07-11 11:15:34 -0400

    created dice roll method

--- src/main/java/com/github/zipcodewilmington/casino/games/highlowdice/HighLowDiceGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/highlowdice/HighLowDiceGame.java
@@ -1,13 +1,21 @@
 package com.github.zipcodewilmington.casino.games.highlowdice;
 
 public class HighLowDiceGame {
-    int sumOfDice = 0;
+    int numOfDice;
     int results;
     int bet;
     String player;
 
 
     public int rollDice() {
-        return 0;
+        int roll = (int) (Math.random() * 6 + 1);
+        return roll;
     }
+     public int tossAndSum(int numOfDice) {
+        int results = 0;
+        for (int i = 0; i < numOfDice; i++) {
+            results += rollDice();
+        }
+        return results;
+     }
 }
