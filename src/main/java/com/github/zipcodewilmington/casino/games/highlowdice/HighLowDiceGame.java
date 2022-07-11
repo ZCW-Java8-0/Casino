2079dd703c44555dce0cca399d34a01fc1c558bd
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-07-11 11:01:28 -0400

    setup

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/highlowdice/HighLowDiceGame.java
@@ -0,0 +1,13 @@
+package com.github.zipcodewilmington.casino.games.highlowdice;
+
+public class HighLowDiceGame {
+    int sumOfDice = 0;
+    int results;
+    int bet;
+    String player;
+
+
+    public int rollDice() {
+        return 0;
+    }
+}
