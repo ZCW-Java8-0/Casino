765fdcc7aff3cd8faec35f71bde2f27ca8e5dea3
kylehwelch <welch@udel.edu>
2022-07-13 14:35:27 -0400

    added jiayong's stuff

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/cardgames/CardRank.java
@@ -0,0 +1,27 @@
+package com.github.zipcodewilmington.casino.games.cardgames;
+
+public enum CardRank {
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+    int value;
+
+    CardRank(int value) {
+        this.value = value;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+}
