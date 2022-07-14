ce7e766cb9e4eae470a9c9bdd71f72225aaa90e7
Chris Kent <Chris_Kent@ymail.com>
2022-07-13 21:20:09 -0400

    Added Card Class

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/deckOfCards.java
@@ -0,0 +1,22 @@
+package com.github.zipcodewilmington;
+import java.util.ArrayList;
+
+public class DeckOfCards {
+    private ArrayList<Card> deck;
+
+
+
+    public DeckOfCards(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+
+
+
+    public DeckOfCards(){
+        List<String> suits =
+
+        List<String> faceNames =
+
+        deck = new ArrayList<Card>();
+    }
+}
